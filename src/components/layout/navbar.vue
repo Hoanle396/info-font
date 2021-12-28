ed783d55b6cb67eb737000c976bf8b1db2fc138f
LÊ HỮU HOÀN <89791546+Hoanle396@users.noreply.github.com>
2021-12-28 16:49:18 +0700

    Update navbar.vue

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -2,7 +2,19 @@
     <nav class="navbar navbar-expand-lg fixed-top" data-aos="fade-up" id="mainNav">
         <div class="container">
             <a class="navbar-brand js-scroll-trigger" href=""><img src="../../assets/logo.png" width="50" /></a>
-            <div >
+             <button
+          class="navbar-toggler navbar-toggler-right"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i class="fas fa-bars ml-1"></i>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav text-uppercase ml-auto">
                     <li class="nav-item"><a class="nav-link text-" href="#index">Trang Đầu</a></li>
                     <li class="nav-item"><a class="nav-link" href="#infomation">Thông Tin</a></li>
